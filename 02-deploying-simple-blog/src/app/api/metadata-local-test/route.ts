0104cc25a2f1450a2b30781120b8e4cbbe5ac37c
Chidiadi Anyanwu <chidiadi01@gmail.com>
2025-05-13 19:00:35 +0100

    removed unecessary folder

--- 02-deploying-simple-blog/src/app/api/metadata-local-test/route.ts
+++
@@ -1,6 +0,0 @@
-import { getMetadataFromLocalFile } from './getMetadataFromLocalFile';
-
-export async function GET() {
-  const result = getMetadataFromLocalFile();
-  return Response.json(result);
-}
