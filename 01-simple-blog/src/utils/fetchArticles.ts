ef4a5a69e7daff7da4badabb77b204aee1e7fadf
Chidiadi Anyanwu <chidiadi01@gmail.com>
2025-07-17 09:20:43 +0100

    Trying out CORS proxy

--- 01-simple-blog/src/utils/fetchArticles.ts
+++ 01-simple-blog/src/utils/fetchArticles.ts
@@ -54,9 +54,7 @@ export async function fetchArticles(): Promise<Article[]> {
         const response = await fetch('https://corsproxy.io/?key=587f5b36&url=' + item.url, {
           headers: {
             'Accept': 'application/xml; charset=utf-8',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Referer': 'https://www.google.com/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
           },
         });
         if (!response.ok) {
