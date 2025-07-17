c04aea7870a3f54abc0c8aeb7e8e203465ff85c9
Chidiadi Anyanwu <chidiadi01@gmail.com>
2025-07-17 09:29:16 +0100

    modified:   01-simple-blog/src/utils/fetchArticles.ts

--- 01-simple-blog/src/utils/fetchArticles.ts
+++ 01-simple-blog/src/utils/fetchArticles.ts
@@ -57,6 +57,7 @@ export async function fetchArticles(): Promise<Article[]> {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
           },
         });
+        
         if (!response.ok) {
           console.error(`HTTP error! Status: ${response.status} for URL: ${item.url}`);
           results.push(null);
